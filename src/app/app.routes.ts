a0d7705861288eddc0ad143ec84f3ab595e5a8b7
Abanobshafeeq <abanobshafeeq100@gmail.com>
2026-01-19 14:02:55 +0200

    adding ACTIVE LINK

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 export const routes: Routes = [
   { path: 'form', component: FormComponent },
